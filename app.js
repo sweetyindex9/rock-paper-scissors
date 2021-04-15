3e7682508a20f8d0b8e608f90b7e13e81b2a1d24
Sweety Jangale <sweety.jangale@indexnine.com>
2021-04-15 11:50:34 +0530

    rock-paper-scissors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const game = () => {
+function game () {
     let pScore = 0;
     let cScore = 0;
   
-    //Start the Game
-    const startGame = () => {
+    
+    function startGame () {
       const playBtn = document.querySelector(".intro button");
       const introScreen = document.querySelector(".intro");
       const match = document.querySelector(".match");
@@ -13,8 +13,8 @@ const game = () => {
         match.classList.add("fadeIn");
       });
     };
-    //Play Match
-    const playMatch = () => {
+    
+    function playMatch() {
       const options = document.querySelectorAll(".options button");
       const playerHand = document.querySelector(".player-hand");
       const computerHand = document.querySelector(".computer-hand");
@@ -25,47 +25,47 @@ const game = () => {
           this.style.animation = "";
         });
       });
-      //Computer Options
+      
       const computerOptions = ["rock", "paper", "scissors"];
   
       options.forEach(option => {
         option.addEventListener("click", function() {
-          //Computer Choice
+          
           const computerNumber = Math.floor(Math.random() * 3);
-          const computerChoice = computerOptions[computerNumber];
+          const computerSelection = computerOptions[computerNumber];
   
           setTimeout(() => {
-            //Here is where we call compare hands
-            compareHands(this.textContent, computerChoice);
-            //Update Images
+            
+            playRound(this.textContent, computerSelection);
+            
             playerHand.src = `./images/${this.textContent}.png`;
-            computerHand.src = `./images/${computerChoice}.png`;
+            computerHand.src = `./images/${computerSelection}.png`;
           }, 2000);
-          //Animation
+          
           playerHand.style.animation = "shakePlayer 2s ease";
           computerHand.style.animation = "shakeComputer 2s ease";
         });
       });
     };
   
-    const updateScore = () => {
+    function updateScore() {
       const playerScore = document.querySelector(".player-score p");
       const computerScore = document.querySelector(".computer-score p");
       playerScore.textContent = pScore;
       computerScore.textContent = cScore;
     };
   
-    const compareHands = (playerChoice, computerChoice) => {
-      //Update Text
+    function playRound(playerSelection, computerSelection) {
+      
       const winner = document.querySelector(".winner");
-      //Checking for a tie
-      if (playerChoice === computerChoice) {
+      
+      if (playerSelection === computerSelection) {
         winner.textContent = "It is a tie";
         return;
       }
-      //Check for Rock
-      if (playerChoice === "rock") {
-        if (computerChoice === "scissors") {
+      
+      if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
           winner.textContent = "Player Wins";
           pScore++;
           updateScore();
@@ -77,9 +77,9 @@ const game = () => {
           return;
         }
       }
-      //Check for Paper
-      if (playerChoice === "paper") {
-        if (computerChoice === "scissors") {
+      
+      if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
           winner.textContent = "Computer Wins";
           cScore++;
           updateScore();
@@ -91,9 +91,9 @@ const game = () => {
           return;
         }
       }
-      //Check for Scissors
-      if (playerChoice === "scissors") {
-        if (computerChoice === "rock") {
+      
+      if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
           winner.textContent = "Computer Wins";
           cScore++;
           updateScore();
@@ -107,10 +107,10 @@ const game = () => {
       }
     };
   
-    //Is call all the inner function
+    
     startGame();
     playMatch();
   };
   
-  //start the game function
+  
   game();
